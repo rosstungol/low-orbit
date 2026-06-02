74131f3239b59a4d594db3284f62aeaeb6f1ef7a
Ross Tungol <rossjstungol@gmail.com>
2026-06-03 03:18:27 +0800

    adds target intersection

--- src/components/Targets.tsx
+++ src/components/Targets.tsx
@@ -1,6 +1,9 @@
+import { useFrame } from '@react-three/fiber'
 import { useMemo, useState } from 'react'
-import { Quaternion, TorusGeometry, Vector3 } from 'three'
+import { type BufferGeometry, Quaternion, TorusGeometry, Vector3 } from 'three'
 import { mergeGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils.js'
+
+import { spaceshipPosition } from './CameraController'
 
 function randomPoint(scale?: Vector3) {
 	return new Vector3(
@@ -20,6 +23,7 @@ export function Targets() {
 			arr.push({
 				center: randomPoint(new Vector3(120, 120, 300)),
 				direction: randomPoint().normalize(),
+				hit: false,
 			})
 		}
 
@@ -27,25 +31,46 @@ export function Targets() {
 	})
 
 	const geometry = useMemo(() => {
-		let geo
+		let geo: BufferGeometry
 
 		targets.forEach((target) => {
-			const torusGeo = new TorusGeometry(TARGET_RAD, 0.2, 80, 25)
+			const torusGeometry = new TorusGeometry(TARGET_RAD, 0.2, 80, 25)
 
-			torusGeo.applyQuaternion(
+			torusGeometry.applyQuaternion(
 				new Quaternion().setFromUnitVectors(
 					new Vector3(0, 0, 1),
 					target.direction
 				)
 			)
-			torusGeo.translate(target.center.x, target.center.y, target.center.z)
+			torusGeometry.translate(target.center.x, target.center.y, target.center.z)
 
-			if (!geo) geo = torusGeo
-			else geo = mergeGeometries([geo, torusGeo])
+			if (!geo) geo = torusGeometry
+			else geo = mergeGeometries([geo, torusGeometry])
 		})
 
 		return geo
 	}, [targets])
+
+	useFrame(() => {
+		targets.forEach((target) => {
+			const v = spaceshipPosition.clone().sub(target.center)
+			const dist = target.direction.dot(v)
+			const projected = spaceshipPosition
+				.clone()
+				.sub(target.direction.clone().multiplyScalar(dist))
+			const hitDist = projected.distanceTo(target.center)
+
+			if (hitDist < TARGET_RAD && Math.abs(dist) < 0.5) {
+				target.hit = true
+			}
+		})
+
+		const targetHit = targets.find((target) => target.hit)
+
+		if (targetHit) {
+			setTargets(targets.filter((target) => !target.hit))
+		}
+	})
 
 	return (
 		<mesh geometry={geometry}>
