b5241c614cf6c2371083a812113c3c8b3e503e8b
Ross Tungol <rossjstungol@gmail.com>
2026-06-05 03:12:35 +0800

    adds start button

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import { useState } from 'react'
 import { SpaceScene } from './components/scene/SpaceScene'
 import { GameControls } from './components/ui/GameControls'
 import { RestartButton } from './components/ui/RestartButton'
+import { StartButton } from './components/ui/StartButton'
 import { TargetCount } from './components/ui/TargetCount'
 
 function App() {
+	const [started, setStarted] = useState<boolean>(false)
 	const [targetsLeft, setTargetsLeft] = useState<number | null>(null)
 
 	return (
 		<main>
 			<GameControls />
+
+			{!started && <StartButton onStart={() => setStarted(true)} />}
+			{started && <SpaceScene setTargetCount={setTargetsLeft} />}
+
 			{targetsLeft != null && <TargetCount targetCount={targetsLeft} />}
 			{targetsLeft === 0 && <RestartButton />}
-			<SpaceScene setTargetCount={setTargetsLeft} />
 		</main>
 	)
 }
